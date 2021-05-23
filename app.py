3fce627fae9047d7c9ad512d559d9e740ec96b66
Ervin Puškar <ervin.puskar@watershedlrs.com>
2021-05-23 13:24:34 -0500

    Better tolerance for lowercase and dashes, better errors, more tests

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from random import choice
 
 app = Flask(__name__)
 
-# according to MBI spec
+# according to MBI spec here, https://www.cms.gov/medicare/new-medicare-card/understanding-the-mbi-with-format.pdf
 # Position 1 – numeric values 1 thru 9
 # Position 2 – alphabetic values A thru Z (minus S, L, O, I, B, Z)
 # Position 3 – alpha-numeric values 0 thru 9 and A thru Z (minus S, L, O, I, B, Z)
@@ -17,20 +17,20 @@ app = Flask(__name__)
 # Position 10 – numeric values 0 thru 9
 # Position 11 – numeric values 0 thru 9
 
+mbiDesignations = {
+    'C': 'a numeric value 1 through 9',
+    'N': 'a numeric value 0 through 9',
+    'A': 'an uppercase alphabetic value A through Z (minus S, L, O, I, B, Z)',
+    'AN': 'an uppercase alpha-numeric value 0 through 9 and A through Z (minus S, L, O, I, B, Z)'
+}
+
+mbiFormat = ['C', 'A', 'AN', 'N', 'A', 'AN', 'N', 'A', 'A', 'N', 'N']
+
 mbiGenerators = {
     'C': [str(i) for i in range(1, 10)],
     'N': [str(i) for i in range(10)],
     'A': [s for s in string.ascii_uppercase if s not in 'SLOIBZ'],
     'AN': [s for s in string.ascii_uppercase if s not in 'SLOIBZ'] + [str(i) for i in range(10)]
-}
-
-mbiFormat = ['C', 'A', 'AN', 'N', 'A', 'AN', 'N', 'A', 'A', 'N', 'N']
-
-mbiDesignations = {
-    'C': 'numeric values 1 through 9',
-    'N': 'numeric values 0 through 9',
-    'A': 'uppercase alphabetic values A through Z (minus S, L, O, I, B, Z)',
-    'AN': 'uppercase alpha-numeric values 0 through 9 and A through Z (minus S, L, O, I, B, Z)'
 }
 
 @app.route('/generate', methods=['GET'])
@@ -46,25 +46,37 @@ def generate():
 def verify():
 
     mbi = request.json.get('mbi')
-    resp = {'valid': False}
+    resp = {'valid': False, 'errors': []}
 
     if mbi:
         # https://stackoverflow.com/questions/47683221/regular-expression-for-medicare-mbi-number-format
         # regexp for quick validation. This seems like cheating and would not allow for returning a meaningful error message.
         # re.search(r"^\d(?![SLOIBZ])[A-Z]\d|(?![SLOIBZ])[A-Z]\d(?![SLOIBZ])[A-Z]\d|(?![SLOIBZ])[A-Z]\d(?![SLOIBZ])[A-Z](?![SLOIBZ])[A-Z]\d\d$", mbi)
 
+        # automatically remove dashes and convert to uppercase, but warn the user
+        if not mbi.isupper():
+            resp['errors'].append('The MBI should use be upper-case letters.')
+        if "-" in mbi:
+            resp['errors'].append('The MBI does NOT include dashes.')
+
+        if len(resp['errors']) > 0:
+            mbi = mbi.upper().replace("-", "")
+            resp['errors'].append(f'This MBI was converted to "{mbi}".')
+
+        # check length first
         if len(mbi) != len(mbiFormat):
-            resp['valid'] = False
             resp['errors'] = [f'The MBI must be exactly {len(mbiFormat)} characters.']
         else:
             resp['valid'] = True
 
+            # accumulate and return ALL formatting errors at once
             for i in range(len(mbiFormat)):
                 if mbi[i] not in mbiGenerators[mbiFormat[i]]:
                     resp['valid'] = False
                     if 'errors' not in resp:
                         resp['errors'] = []
-                    resp['errors'].append(f'{i + 1}{ {0: "st", 1: "nd", 2: "rd"}[i] if i < 3 else "th" } character must be {mbiDesignations[mbiFormat[i]]}')
+                    resp['errors'].append(f'{i + 1}{ {0: "st", 1: "nd", 2: "rd"}[i] if i < 3 else "th" } '
+                                          f'character must be {mbiDesignations[mbiFormat[i]]}')
 
     return jsonify(resp)
 
--- test_mbi.py
+++ test_mbi.py
@@ -6,12 +6,32 @@ def test_get():
 
 
 def test_verify():
-    app.request = {'json': {'mbi': '5Y20MA2KW16'}}
-    r = app.verify()
-    assert r == True
+    with app.app.test_client() as c:
+        rv = c.post('/verify', json={
+            'mbi': '5Y20MA2KW16'
+        })
+        assert rv.get_json()['valid'] == True
+
+        rv = c.post('/verify', json={
+            'mbi': '5Y20MA2KW6'
+        })
+        assert rv.get_json()['valid'] == False
+
+        rv = c.post('/verify', json={
+            'mbi': '5Y20mA2Kw16'
+        })
+        assert rv.get_json()['valid'] == True
+
+        rv = c.post('/verify', json={
+            'mbi': '5Y20-mA2-Kw16'
+        })
+        assert rv.get_json()['valid'] == True
 
 def test_generate_then_verify():
-    r = app.generate()
-    app.request = {'json': {'mbi': r}}
-    r = app.verify()
-    assert r == True
+    with app.app.test_client() as c:
+        rv = c.get('/generate')
+
+        rv = c.post('/verify', json={
+            'mbi': str(rv.get_data().decode('UTF-8'))
+        })
+        assert rv.get_json()['valid'] == True
